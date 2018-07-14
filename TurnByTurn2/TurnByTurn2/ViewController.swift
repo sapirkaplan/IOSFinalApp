490fe9f9ac90eba46e7af21d55959169faed7ca8
sapirkaplan <sapirkaplan@gmail.com>
2018-07-14 18:49:27 +0300

    Add files via upload
    1

---
+++ TurnByTurn2/TurnByTurn2/ViewController.swift
@@ -0,0 +1,199 @@
+//
+//  ViewController.swift
+//  TurnByTurn2
+//
+//  Created by Sapir Kaplan on 10/07/2018.
+//  Copyright © 2018 Sapir Kaplan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var directionsLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBAction func startBtn(_ sender: UIButton) { showDirections() }
+    @IBOutlet weak var timeLeftLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    var currentCoordinate: CLLocationCoordinate2D!
+    var steps = [MKRouteStep]()
+    let speechSyntheizer = AVSpeechSynthesizer()
+    var stepCounter = 1
+    var timer:Timer!
+    var countDown = 90
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.startUpdatingLocation()
+    }
+    
+    @objc
+    func countDownAction(){
+        countDown -= 1
+        timeLeftLabel.text = "Time left: \(countDown)"
+        if countDown == 0 {
+            timer.invalidate()
+        }
+    }
+    
+    func showDirections() {
+        let direction = setDirectionsValues()
+        
+        direction.calculate { (response, _) in
+            guard let response = response else {return}
+            guard let primaryRoute = response.routes.first else {return}
+            self.mapView.add(primaryRoute.polyline) // Call the renderer extension
+            
+            //Get fresh directions
+            //To avoid doubles
+            self.locationManager.monitoredRegions.forEach({self.locationManager.stopMonitoring(for: $0)})
+            
+            //Turn by turn directions
+            self.turnByTurnDirections(primaryRoute: primaryRoute)
+            
+            //Set directions
+            self.setDirections()
+        }
+        
+        //Set time left
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDownAction), userInfo: nil, repeats: true)
+    }
+    
+    func setDirectionsValues() -> MKDirections {
+        //This will make zoom in automaticly when running the application
+        let region = MKCoordinateRegion(center: currentCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+        mapView.setRegion(region, animated: true)
+        
+        let directionsRequest = MKDirectionsRequest()
+        
+        directionsRequest.source = source()
+        directionsRequest.destination = destination()
+        directionsRequest.transportType = .walking
+        
+        return MKDirections(request: directionsRequest)
+    }
+    
+    func source() -> MKMapItem {
+        let sourcePlaceMark = MKPlacemark(coordinate: currentCoordinate)
+        return MKMapItem(placemark: sourcePlaceMark)
+    }
+    
+    func destination() -> MKMapItem {
+        let destinationCoordinate = CLLocationCoordinate2D(latitude: 37.7879, longitude: -122.4102) //The Donatello hotel San Francisco coordinates
+        
+        //Create annotation for destination
+        let destinationAnnotation = Annotation(coordinate: destinationCoordinate, title: "Safe Place", subtitle: "bla bla")
+        mapView.addAnnotation(destinationAnnotation)
+        
+        let destinationPlaceMark = MKPlacemark(coordinate: destinationCoordinate)
+        return MKMapItem(placemark: destinationPlaceMark)
+    }
+    
+    func turnByTurnDirections(primaryRoute:MKRoute!){
+        steps = primaryRoute.steps
+        for i in 0..<primaryRoute.steps.count{
+            let step = primaryRoute.steps[i]
+            let region = CLCircularRegion(center: step.polyline.coordinate, radius: 20, identifier: "\(i)")
+            locationManager.startMonitoring(for: region)
+            
+            //Show the directions instructions
+            let circle = MKCircle(center: region.center, radius: region.radius)
+            mapView.add(circle)
+        }
+    }
+    
+    func setDirections(){
+        //Set text directions
+        let initialMessage = "In \(steps[1].distance) meters, \(steps[1].instructions) then in \(steps[2].distance) meters, \(steps[2].instructions)."
+        directionsLabel.text = initialMessage
+        
+        //Set speech directions
+        setSpeechDirections(initialMessage: initialMessage)
+    }
+    
+    func setSpeechDirections(initialMessage:String) {
+        let speechUtterance = AVSpeechUtterance(string: initialMessage)
+        self.speechSyntheizer.speak(speechUtterance)
+        self.stepCounter += 1
+    }
+}
+
+//This extension will happen when updating location from viewDidLoad - to get user current location
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+        
+        //Get user current location
+        guard let currentLocation = locations.first else { return }
+        currentCoordinate = currentLocation.coordinate
+        
+        //Zoom in to user location
+        mapView.userTrackingMode = .followWithHeading //The followWithHeading means that when ever you move the phone it will point the map in the direction that you are looking instead of just pointing it to north
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("enter")
+        stepCounter += 1
+        if stepCounter < steps.count{
+            let currentStep = steps[stepCounter]
+            let message = "In \(currentStep.distance) meters, \(currentStep.instructions)"
+            directionsLabel.text = message
+            let speechUtterance = AVSpeechUtterance(string: message)
+            speechSyntheizer.speak(speechUtterance)
+        }
+        else{
+            let message = "Arrived to destination"
+            directionsLabel.text = message
+            let speechUtterance = AVSpeechUtterance(string: message)
+            speechSyntheizer.speak(speechUtterance)
+            stepCounter = 1
+            locationManager.monitoredRegions.forEach({self.locationManager.stopMonitoring(for: $0)})
+        }
+    }
+}
+
+//This extension will happen when we want to display things on the screen
+extension ViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        //This extension will happen when we want to display the location on the screen
+        if overlay is MKPolyline{
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .blue
+            renderer.lineWidth = 7
+            return renderer
+        }
+        
+        //This extension will happen when we want to display the directions on the screen
+        if overlay is MKCircle{
+            let renderer = MKCircleRenderer(overlay: overlay)
+          //  renderer.strokeColor = .red
+//            renderer.fillColor = .red
+//            renderer.alpha = 0.5
+            return renderer
+        }
+        return MKOverlayRenderer()
+    }
+}
+
+
+//Create an annotation object
+final class Annotation: NSObject, MKAnnotation{
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?){
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+        
+        super.init()
+    }
+}
